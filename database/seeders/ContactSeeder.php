ccf03c915551bf087100ac55d7fd095394dfc88c
mariakhachatryann <mariakhachatryan23@gmail.com>
2025-10-11 15:31:49 +0400

    seed

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Contact::create([
+            'address' => 'address',
+            'phone' => 'phone',
+            'social_links' => [
+            ],
+            'logo' => '',
+        ]);
+    }
+}
+
